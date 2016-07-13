a6691efda88b35c06c7f1d08157e7930d1ccd2d2
Victoria Huang <victoriahuang107@yahoo.com>
2016-07-13 17:06:00 -0400

    Update DisplayWindow.java

--- DisplayWindow.java
+++ DisplayWindow.java
@@ -9,17 +9,29 @@ import javax.swing.JScrollPane;
 public class DisplayWindow {
 	
 	public static void main(String[] args) {
-		JEditorPane editor = new JEditorPane();
-		editor.setEditable(false);   
+		JEditorPane editor = new JEditorPane(); // the thing that the HTML file appears in
+		editor.setEditable(false);				// so that no weird things appear on the screen (set to true to see a funny html page)
 		
 		try {
+			
+			/**
+			 * uncomment these 2 lines later to load HTML from a local file (to change later - passed in via args in other methods)
+			 */
 			//File file = new File("C://Users//Victoria//Documents//brookhaven//home.php");
-			//jep.setPage(file.toURI().toURL());
-			editor.setPage("http://www.google.com");
+			//editor.setPage(file.toURI().toURL());
+			
+			
+			
+			editor.setPage("http://www.google.com"); // remove later - html of website we want to see
 		}catch (IOException e) {
 			editor.setContentType("text/html");
 			editor.setText("<html>Could not load</html>");
 		} 
+		
+		/**
+		 * regular GUI stuff to make the things appear on the screen
+		 * 
+		 */
 		
 		JScrollPane scrollPane = new JScrollPane(editor);
 		scrollPane.setPreferredSize(new Dimension(800, 600));
