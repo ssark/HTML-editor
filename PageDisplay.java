6043c24768d73527b978ac32bd3a39f84507a9d2
Victoria Huang <victoriahuang107@yahoo.com>
2016-07-14 19:08:13 -0400

    Update PageDisplay.java
    added javadoc comments, handled case where file is null (nothing is chosen)

--- PageDisplay.java
+++ PageDisplay.java
@@ -15,22 +15,40 @@ import javax.swing.JEditorPane;
 public class PageDisplay extends JEditorPane {
 	private File file;
 	
+	/**
+	 * Set and display HTML page
+	 * 
+	 * @param file	page to be loaded
+	 */
 	public PageDisplay(File file) {
 		updateFile(file);
 		setEditable(false);	// so that no weird things appear on the screen (set to true to see a funny html page)
 		display();
 	}
 	
+	/**
+	 * Sets new page to be displayed
+	 * @param file	new page to be displayed
+	 */
 	public void updateFile(File file) {
 		this.file = file;
 	}
 	
+	/**
+	 * Displays HTML page or gives error screen if file cannot be loaded
+	 * Prompts to choose a file if file is null
+	 */
 	public void display() {
+		if (file == null) {
+			setContentType("text/html");
+			setText("<html><h1>Please select a HTML file to load<h1></html>");
+			return;
+		}
 		try {
 			setPage(file.toURI().toURL());
 		}catch (IOException e) {
 			setContentType("text/html");
-			setText("<html>Could not load</html>");
+			setText("<html><h1>Error!<h1><h2>Page could not load<h2></html>");
 		}
 	}
 }
